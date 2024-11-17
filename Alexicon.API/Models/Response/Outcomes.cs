f0c7aeba252ab730ac8c0f8426bba215d8972a58
Black Box <developer.luke.barker3@outlook.com>
2024-11-17 22:39:21 +0000

    Start defining outcome types

---
+++ Alexicon.API/Models/Response/Outcomes.cs
@@ -0,0 +1,24 @@
+namespace Alexicon.API.Models.Response;
+
+public static class Outcomes
+{
+    public const string Ok = "Ok";
+
+    public const string Created = "Created";
+
+    public const string Updated = "Updated";
+
+    public const string EndpointDoesNotExist = "Endpoint Does Not Exist";
+    
+    public const string InvalidContentType = "InvalidContentType";
+
+    public const string MethodNotAllowed = "MethodNotAllowed";
+
+    public const string ModelBindingFailed = "ModelBindingFailed";
+
+    public const string ValidationFailed = "ValidationFailed";
+    
+    public const string NotFound = "NotFound";
+    
+    public const string InternalServerError = "InternalServerError";
+}
