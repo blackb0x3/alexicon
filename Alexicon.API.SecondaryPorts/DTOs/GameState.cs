45737262b19c045ef560ece89f7e1c1ffc0fc5d0
Black Box <developer.luke.barker3@outlook.com>
2024-11-20 00:56:50 +0000

    Remove Board property from game DTO

--- Alexicon.API.SecondaryPorts/DTOs/GameState.cs
+++ Alexicon.API.SecondaryPorts/DTOs/GameState.cs
@@ -7,13 +7,10 @@ public class GameState
         MovesPlayed = new List<GameMove>();
     }
 
-    public byte[,] Board { get; set; }
-
     public List<GameMove> MovesPlayed { get; set; }
 
     public static GameState NewGame => new()
     {
-        Board = new byte[15, 15],
         MovesPlayed = new List<GameMove>()
     };
 }
