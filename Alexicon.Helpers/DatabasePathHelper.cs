e16f3589c867a6d7822db26a5d062586fc9da08b
Black Box <developer.luke.barker3@outlook.com>
2024-11-21 20:06:15 +0000

    Create DatabasePathHelper

---
+++ Alexicon.Helpers/DatabasePathHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace Alexicon.Helpers;
+
+public class DatabasePathHelper
+{
+    private const string DbFileName = "firebotproxy.db";
+
+    public static string DbFilePath => Path.Join(UserHomeFolder, DbFileName);
+
+    public static string GetSqliteConnectionString() => $"Data Source={DbFilePath}";
+
+    private static string UserHomeFolder => Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+}
