756b3f492fb256584aa36384bca6cea593be8b49
Black Box <developer.luke.barker3@outlook.com>
2024-11-21 23:05:35 +0000

    Install EF Core and Mapster type adapters

--- Alexicon.API.Infrastructure/IoC/InfrastructureInstaller.cs
+++ Alexicon.API.Infrastructure/IoC/InfrastructureInstaller.cs
@@ -1,3 +1,8 @@
+using Alexicon.API.Infrastructure.DataAccess;
+using Alexicon.Helpers;
+using EntityFramework.Exceptions.Sqlite;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,5 +12,31 @@ public class InfrastructureInstaller
 {
     public static void Install(IServiceCollection services, IConfiguration config)
     {
+        services.AddDbContext<AlexiconContext>(options =>
+        {
+            options.UseSqlite(DatabasePathHelper.GetSqliteConnectionString())
+                .UseExceptionProcessor();
+        });
+
+        AddMapsterConfigurations();
+    }
+
+    private static void AddMapsterConfigurations()
+    {
+        TypeAdapterConfig<Entities.GamePlayer, SecondaryPorts.DTOs.GamePlayer>.NewConfig()
+            .Map(dest => dest.Player, src => src.Player)
+            .Map(dest => dest.CurrentRack, src => src.CurrentRack)
+            .PreserveReference(false);
+
+        TypeAdapterConfig<Entities.GameMove, SecondaryPorts.DTOs.GameMove>.NewConfig()
+            .Map(dest => dest.Player, src => src.Player)
+            .Map(dest => dest.LettersUsed, src => src.LettersUsed)
+            .Map(dest => dest.WordsCreated, src => src.WordsCreated)
+            .PreserveReference(false);
+
+        TypeAdapterConfig<Entities.Game, SecondaryPorts.DTOs.Game>.NewConfig()
+            .Map(dest => dest.Players, src => src.Players.ToList().AsReadOnly())
+            .Map(dest => dest.MovesPlayed, src => src.MovesPlayed.ToList().AsReadOnly())
+            .PreserveReference(false);
     }
 }
