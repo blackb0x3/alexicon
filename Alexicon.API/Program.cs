5decb6ade8f0f309bc6676e4ac90e4c1b1ccac8f
Black Box <developer.luke.barker3@outlook.com>
2024-11-22 00:33:07 +0000

    Call ApiInstaller and add Swagger UI descriptions

--- Alexicon.API/Program.cs
+++ Alexicon.API/Program.cs
@@ -1,5 +1,10 @@
+using System.Text.Json;
 using Alexicon.API.Domain.PrimaryPorts.CreateGame;
+using Alexicon.API.Domain.Representations;
+using Alexicon.API.Domain.Representations.Games;
+using Alexicon.API.IoC;
 using Alexicon.API.Models.Requests;
+using Alexicon.API.Models.Response;
 using MapsterMapper;
 using Mediator;
 using Microsoft.AspNetCore.Mvc;
@@ -11,6 +16,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.Configure<JsonOptions>(opts =>
+{
+    opts.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+    opts.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+});
+ApiInstaller.Install(builder.Services, builder.Configuration);
 
 if (builder.Environment.IsDevelopment())
 {
@@ -28,7 +39,9 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.MapPost("/game",
-        [SwaggerOperation("")]
+        [SwaggerOperation("Create a new Scrabble game.")]
+        [SwaggerResponse(StatusCodes.Status200OK, Outcomes.Ok, typeof(GameRepresentation))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, Outcomes.ValidationFailed, typeof(ValidationRepresentation))]
         async (
             HttpRequest httpReq,
             [FromServices] IMediator mediator,
@@ -39,6 +52,11 @@ app.MapPost("/game",
             var request = mapper.Map<CreateGameRequest>(req);
 
             var response = await mediator.Send(request);
+
+            return response.Match<IResult>(
+                created => Results.Ok(created),
+                invalidRequest => Results.BadRequest(invalidRequest)
+            );
         })
     .WithName("StartNewGame")
     .WithOpenApi();
