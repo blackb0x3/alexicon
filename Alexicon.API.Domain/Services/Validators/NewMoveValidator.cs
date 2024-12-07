0b1457307183ace62d6d505d1af0bafb042b0d25
Black Box <developer.luke.barker3@outlook.com>
2024-12-07 20:12:26 +0000

    Implement GetNewWords

--- Alexicon.API.Domain/Services/Validators/NewMoveValidator.cs
+++ Alexicon.API.Domain/Services/Validators/NewMoveValidator.cs
@@ -45,7 +45,117 @@ public class NewMoveValidator : INewMoveValidator
 
     private List<WordCreated> GetNewWords(List<char> letters, List<(short, short)> path, byte[,] board)
     {
-        throw new NotImplementedException();
+        var wordsCreated = new List<WordCreated>();
+        var primaryWord = BuildWordFromPath(path, letters, board);
+        wordsCreated.Add(primaryWord);
+
+        // Check for secondary words formed by letters in orthogonal directions
+        foreach (var (x, y) in path)
+        {
+            var secondaryWord = BuildSecondaryWord(x, y, board);
+            if (secondaryWord != null)
+            {
+                wordsCreated.Add(secondaryWord);
+            }
+        }
+
+        return wordsCreated;
+    }
+
+    private WordCreated BuildWordFromPath(List<(short, short)> path, List<char> letters, byte[,] board)
+    {
+        var word = new char[path.Count];
+        var letterIndex = 0;
+
+        for (var i = 0; i < path.Count; i++)
+        {
+            var (x, y) = path[i];
+
+            // when space is already being occupied
+            if (board[y, x] != 0)
+            {
+                word[i] = (char)board[y, x];
+            }
+
+            // when space is empty
+            else
+            {
+                word[i] = letters[letterIndex++];
+            }
+        }
+
+        return new WordCreated
+        {
+            Word = new string(word),
+            IsValid = IsValidWord(new string(word)), // Assume ValidateWord is implemented elsewhere
+            Reason = IsValidWord(new string(word)) ? null : "InvalidWord"
+        };
+    }
+
+    private WordCreated? BuildSecondaryWord(short x, short y, byte[,] board)
+    {
+        var horizontalWord = BuildWordInDirection(x, y, board, 0, -1, 0, 1); // Left and right
+        var verticalWord = BuildWordInDirection(x, y, board, -1, 0, 1, 0); // Up and down
+
+        if (horizontalWord != null && horizontalWord.Word.Length > 1)
+        {
+            return horizontalWord;
+        }
+
+        if (verticalWord != null && verticalWord.Word.Length > 1)
+        {
+            return verticalWord;
+        }
+
+        return null;
+    }
+
+    private WordCreated? BuildWordInDirection(short startX, short startY, byte[,] board, short dx1, short dy1, short dx2, short dy2)
+    {
+        var word = new List<char>();
+        var (x, y) = (startX, startY);
+
+        // Move in the first direction
+        while (x >= 0 && y >= 0 && x < board.GetLength(0) && y < board.GetLength(1) && board[x, y] != 0)
+        {
+            word.Insert(0, (char)board[x, y]);
+            x += dx1;
+            y += dy1;
+        }
+
+        // Reset to the original position and move in the second direction
+        x = startX;
+        y = startY;
+
+        while (x >= 0 && y >= 0 && x < board.GetLength(0) && y < board.GetLength(1) && board[x, y] != 0)
+        {
+            if ((x != startX || y != startY)) // Avoid adding the center tile twice
+            {
+                word.Add((char)board[x, y]);
+            }
+
+            x += dx2;
+            y += dy2;
+        }
+
+        if (word.Count > 1)
+        {
+            var wordString = new string(word.ToArray());
+            return new WordCreated
+            {
+                Word = wordString,
+                IsValid = IsValidWord(wordString),
+                Reason = IsValidWord(wordString) ? string.Empty : "InvalidWord"
+            };
+        }
+
+        return null;
+    }
+
+    private bool IsValidWord(string word)
+    {
+        // TODO Implement the dictionary-based validation here
+        return true;
     }
 
     private static HashSet<(short, short)> GetIntersectableTileIndices(byte[,] board)
