c0c1907bc4e3f9590d81822ef26d0fc6244e24fc
Black Box <developer.luke.barker3@outlook.com>
2024-12-29 23:43:47 +0000

    Add JsonConverter for Location prop

--- Alexicon.API.Domain/Representations/Games/GameMoveRepresentation.cs
+++ Alexicon.API.Domain/Representations/Games/GameMoveRepresentation.cs
@@ -1,13 +1,17 @@
+using System.Text.Json.Serialization;
+using Alexicon.API.Domain.Services.Converters;
+
 namespace Alexicon.API.Domain.Representations.Games;
 
 public class GameMoveRepresentation
 {
     public string PlayerUsername { get; set; }
-    
+
     public List<char> LettersUsed { get; set; }
-    
+
     public List<string> WordsCreated { get; set; }
-    
+
+    [JsonConverter(typeof(ValueTupleJsonConverter<string, string>))]
     public (string, string) Location { get; set; }
 
     public short Score { get; set; }
